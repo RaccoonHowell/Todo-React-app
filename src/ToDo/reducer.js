de3e3afd682545a46c53201d2346323bcb28e35e
Rachel <rac777blue@hotmail.com>
2020-03-12 14:24:42 +0000

    added tests to the add items section of the reducer testing  file and added an add item function to the reducer file and did tdd until all test in that section passed

--- src/ToDo/reducer.js
+++ src/ToDo/reducer.js
@@ -1,5 +1,32 @@
 import React, { useReducer } from "react";
 
+const initial = {
+    items: [
+        { task: "Say hello", completed: true },
+        { task: "Say goodbye", completed: false },
+    ]
+}
+
+const reducer= () => {
+
+}
+
 export const addItem = (state, { value }) => {
-    // … your code here
+    return {
+        ...state,
+        items: [
+            ...state.items,
+            { task: value, completed: false }
+        ]
+    }
 };
+
+export default reducer;
+// Our state will consist of an array, items, containing object literals with a task and completed property. For example:
+
+// {
+//     items: [
+//         { task: "Say hello", completed: true },
+//         { task: "Say goodbye", completed: false },
+//     ]
+// }
